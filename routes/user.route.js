a32e380b8ebb48ea01aebf24683a19e1bf778cc9
yashwanth-elocity <yashwanth@elocitytech.com>
2025-01-26 13:18:11 +0530

    GET /api/tasks/:id and DELETE /api/tasks/:id Implemented

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 const Task = require("../models/task.model");
 const userAuthenticated = require("../middlewares/userAuthenticated");
 const { checkTaskData } = require("../utils/validations");
+const mongoose = require("mongoose");
 // console.log(userAuthenticated);
 
 userRouter.post("/api/tasks", userAuthenticated, async (req, res) => {
@@ -27,6 +28,43 @@ userRouter.post("/api/tasks", userAuthenticated, async (req, res) => {
       message: "Unexpected error occured",
       errors: { list: error.message.split("`") },
     });
+  }
+});
+
+userRouter.get("/api/tasks/:id", userAuthenticated, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // const taskExists = await Task.findById({ _id: id });
+    const taskExists = await Task.findById(new mongoose.Types.ObjectId(id));
+    if (!taskExists) {
+      return res
+        .status(404)
+        .json({ message: "Task Not found", data: taskExists });
+    }
+
+    res.json({ message: "ok", data: taskExists });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+userRouter.delete("/api/tasks/:id", userAuthenticated, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // check if tasks exists to delete it
+    const task = await Task.findOne({ _id: id });
+    if (!task) {
+      return res.status(404).json({ message: "Task Doesn't Exist To delete" });
+    }
+
+    const deletedTask = await Task.findByIdAndDelete({ _id: id });
+    res.json({ message: "ok", data: deletedTask });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Unexpected Error Occured", error: error.message });
   }
 });
 
